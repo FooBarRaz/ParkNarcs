ffd033173e4e1afc5d2485a3835153113524c5c7
Raz Ali <mehraz.hakim@gmail.com>
2022-02-03 15:42:30 -0500

    add react-crop library to crop image before upload

--- src/features/narc/submission/ImagePreview.tsx
+++ src/features/narc/submission/ImagePreview.tsx
@@ -1,6 +1,9 @@
 // @flow
 import * as React from 'react';
 import { Box } from '@mui/material';
+import ReactCrop from 'react-image-crop';
+import { useEffect, useState } from 'react';
+import 'react-image-crop/dist/ReactCrop.css';
 
 type Props = {
     src: string;
@@ -8,6 +11,12 @@ type Props = {
 
 export const ImagePreview = (props: Props) => {
     const {src} = props;
+    const [crop, setCrop] = useState<any>();
+
+    useEffect(() => {
+        console.log(crop);
+    }, [crop]);
+
     return <Box
         sx={{
             paddingTop: '1rem',
@@ -15,11 +24,18 @@ export const ImagePreview = (props: Props) => {
             width: '50%'
         }}
     >
-        {src && <img
+        {src &&
+        <ReactCrop
             src={src}
-            alt="preview"
-            width="100%"
-        />}
+            crop={crop}
+            onChange={setCrop}/>
+
+            // <img
+            //     src={src}
+            //     alt="preview"
+            //     width="100%"
+            // />
+        }
     </Box>
 
 };
