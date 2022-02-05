702eb0c9586604860d65b3e004565ccef82f5f4a
Raz Ali <mehraz.hakim@gmail.com>
2022-02-04 21:38:32 -0500

    use environment variable for image width

--- amplify/backend/function/S3Trigger30c01cf3/src/imageResizer.js
+++ amplify/backend/function/S3Trigger30c01cf3/src/imageResizer.js
@@ -1,6 +1,8 @@
 const sharp = require('sharp');
 
 const resizeImage = (img) => {
+    const { IMAGE_WIDTH } = process.env;
+    console.log('image width: ', IMAGE_WIDTH);
     return sharp(img).resize({ width: 780 })
         .webp()
         .toBuffer()
