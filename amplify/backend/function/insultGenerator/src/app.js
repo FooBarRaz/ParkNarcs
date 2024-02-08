3ec7d56aca1a8d01f3470953fc19290161611f52
Raz Ali <mehraz.hakim@gmail.com>
2024-02-07 20:14:30 -0500

    Add openAPI insult generator API

---
+++ amplify/backend/function/insultGenerator/src/app.js
@@ -0,0 +1,89 @@
+/*
+Use the following code to retrieve configured secrets from SSM:
+
+Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
+*/
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+const express = require("express");
+const bodyParser = require("body-parser");
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware");
+const aws = require("aws-sdk");
+
+// declare a new express app
+const app = express();
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+const getOpenAiApiKey = async () => {
+  const { Parameters } = await new aws.SSM()
+    .getParameters({
+      Names: ["OPEN_AI_API_KEY"].map((secretName) => process.env[secretName]),
+      WithDecryption: true,
+    })
+    .promise();
+
+  const openAiApiKey = Parameters.find(
+    (param) => param.Name === "OPEN_AI_API_KEY"
+  ).Value;
+
+  return openAiApiKey;
+};
+
+
+getOpenAiApiKey().then((openAiApiKey) => {
+  process.env.OPEN_AI_API_KEY = openAiApiKey;
+});
+
+// Enable CORS for all methods
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+/**********************
+ * Example get method *
+ **********************/
+const { Configuration, OpenAIApi } = require("@openai/client");
+
+app.get("/", async (req, res) => {
+  let openAiApiKey = process.env.OPEN_AI_API_KEY;
+
+  if (!openAiApiKey) {
+    openAiApiKey = await getOpenAiApiKey();
+  }
+
+  const configuration = new Configuration({
+    apiKey: openAiApiKey,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003", // Check for the latest model when implementing
+      prompt: "Generate a humorous insult for bad parking:",
+      temperature: 0.7,
+      max_tokens: 60,
+    });
+
+    res.json({ insult: response.data.choices[0].text.trim() });
+  } catch (error) {
+    console.error("Error calling OpenAI API:", error);
+    res.status(500).send("Failed to generate insult");
+  }
+});
+
+app.listen(3000, function () {
+  console.log("App started");
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app;
--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -2,7 +2,6 @@
 import { generateClient } from "aws-amplify/api/server";
 import { cookies } from "next/headers";
 import { runWithAmplifyServerContext } from "./amplifyServerUtils";
-import { post } from "aws-amplify/api";
 
 export async function fetchPosts() {
   const listPostsQuery = `query ListPosts {
@@ -41,38 +40,3 @@ export async function fetchPosts() {
     }
   });
 }
-
-// export async function handleImageUpload(formData: FormData) {
-//   console.log("handleImageUpload");
-//   const image = formData.get("image");
-//   if (image) {
-//     const extension = "png";
-//     const filename = `${Date.now()}-${formData.get("title")}.${extension}`;
-
-//     try {
-//       const response = await runWithAmplifyServerContext({
-//         nextServerContext: { cookies },
-//         operation: (context) => {
-//           const encodedFormData = btoa(JSON.stringify(formData));
-//           return post(context, {
-//             apiName: "parkNarcsREST",
-//             path: "/posts",
-//             options: {
-//               body: encodedFormData,
-//               headers: {
-//                 "Content-Type": "multipart/form-data",
-//               },
-//             },
-//           }).response;
-//         },
-//       });
-//       await response.body.json().then((data) => console.log(data));
-//     } catch (error) {
-//       console.log("Error : ", error);
-//     }
-//     revalidatePath("/posts/create");
-//     redirect("/");
-//   } else {
-//     console.log("No image");
-//   }
-// }
